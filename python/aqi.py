0e038766dbd1bec59b2f9ab083eb5d546f67dcd2
David <2524133+dcato98@users.noreply.github.com>
2022-04-28 06:48:28 -0400

    Update aqi.py

--- python/aqi.py
+++ python/aqi.py
@@ -37,8 +37,8 @@ def construct_command(cmd, data=[]):
     assert len(data) <= 12
     data += [0,]*(12-len(data))
     checksum = (sum(data)+cmd-2)%256
-    ret = [0xaa, 0xb4, cmd] + [x for x in data] + [0xff, 0xff, checksum, 0xab]
-    ret = ''.join(chr(x) for x in ret)
+    ret = [\xaa, \xb4, chr(cmd)] + [chr(x) for x in data] + [\xff, \xff, chr(checksum), \xab]
+#    ret = ''.join(chr(x) for x in ret)
     ret = ret.encode()
     
     print(type(ret), ret)
