2e412ef489d9aaf6e58d0f40333f1a6539fd2479
David <2524133+dcato98@users.noreply.github.com>
2022-04-28 06:51:08 -0400

    Update aqi.py

--- python/aqi.py
+++ python/aqi.py
@@ -39,7 +39,7 @@ def construct_command(cmd, data=[]):
     checksum = (sum(data)+cmd-2)%256
     ret = ['\xaa', '\xb4', chr(cmd)] + [chr(x) for x in data] + ['\xff', '\xff', chr(checksum), '\xab']
     ret = ''.join(x for x in ret)
-    ret = ret.encode()
+    ret = ret.encode('ascii')
     
     print(type(ret), ret)
 
